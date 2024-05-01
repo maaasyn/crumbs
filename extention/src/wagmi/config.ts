c1b963252c32e7820219714a6ecfab6c3724c76a
Marcin Wajdzik <wajdzik.martin@gmail.com>
2024-05-01 23:27:22 +0200

    chore: garbage out

--- extention/src/wagmi/config.ts
+++
@@ -1,9 +0,0 @@
-import { createConfig, http } from "wagmi";
-import { sepolia } from "wagmi/chains";
-
-export const config = createConfig({
-  chains: [sepolia],
-  transports: {
-    [sepolia.id]: http(),
-  },
-});
